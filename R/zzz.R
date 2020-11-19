3096fc24e5dcd8ac63d82ec53ad3408ab1fe9a12
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-19 22:27:15 +1100

    pkg now loads gtfs-realtime.proto on load

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onLoad <- function(libname, pkgname) {
+
+  proto.dir <- system.file("proto", package = "tfnswapi", mustWork = T)
+  proto.file <- file.path(proto.dir, "gtfs-realtime.proto")
+  RProtoBuf::readProtoFiles(proto.file)
+
+}
