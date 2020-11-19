34dc9300e51c8a1ce020aa48609e403b6f2cb96f
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-19 22:28:32 +1100

    generalise tfnswapi_get, it now can parse gtfsr protobuf

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -3,3 +3,9 @@ test_that("tfnswapi_get works", {
   response = tfnswapi_get(api = "carpark", params = list(facility = 2))
   checkmate::expect_list(response, names = "unique")
 })
+
+test_that("gtfsr", {
+  skip_on_ci()
+  response = tfnswapi_get("gtfs/realtime/buses")
+  checkmate::expect_class(response, "tfnswapi")
+})
